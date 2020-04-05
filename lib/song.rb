e826f09bd2d04228fdb1aac6fc0eadc616ae452c
Ashley Etemadi <ash.etemadi@gmail.com>
2020-04-05 00:15:51 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@artist_count = {}
+  @@genre_count = {}
+  
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist
+    @genre = genre 
+    @@count += 1  
+    @@artists << artist 
+    @@genres << genre 
+  end 
+
+  def self.count 
+    @@count.length 
+  end 
+  
+  def self.artists
+    @@artists 
+  end 
+end 
